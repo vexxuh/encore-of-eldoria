24eb4427a34f439f6970b45b14c7c4539dfa29a5
shayne-tuel <142040826+shayne-tuel@users.noreply.github.com>
2024-04-28 18:35:05 -0400

    added ai text gen apis
    Added the ability to query to the text gen api. Simple to start but as we build this out over the next week will have a more content and this code will need to be refactored for that! No big deal!

---
+++ game-core/ai/novel-ai/novel-api.go
@@ -0,0 +1,52 @@
+package novel_ai
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type novelResponseBody struct {
+	Output string `json:"output"`
+}
+
+func GenerateAiText(input string) (string, error) {
+	client := http.DefaultClient
+
+	requestBody := buildTextAiRequest(input)
+	// Create an HTTP GET request
+	req, err := http.NewRequest("POST", "https://api.novelai.net/ai/generate", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return "", errors.New("could not build request")
+	}
+
+	// Set any headers if required
+	req.Header.Set("Authorization", "Bearer pst-pWHkb2My1LFT0bEsPF4ZIw0aKy162caPNH2qCxs5hB7z7ZwVuaEHq7nRhE2EhVTm")
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the request and get the response
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", errors.New("could not request the novel api for ai/textgen")
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.New("could read response body")
+	}
+
+	// Parse the JSON response body into an instance of MyResponse struct
+	var novelResp novelResponseBody
+	if err := json.Unmarshal(responseBody, &novelResp); err != nil {
+		return "", errors.New("could not parse response body")
+	}
+
+	return novelResp.Output, nil
+}
