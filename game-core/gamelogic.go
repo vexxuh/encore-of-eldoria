1326da7342afb23118a32f8052ca0128e14b54a6
Dragoon209 <david.gaither@gmail.com>
2024-04-27 23:09:34 -0400

    game structure layout

--- game-core/gamelogic.go
+++ game-core/gamelogic.go
@@ -1,1 +1,59 @@
 package gamecore
+import("fmt")
+
+func main(){
+	fmt.Println("Yo!")
+
+
+
+}
+
+// functions
+	// command parser
+		// playerID, command, arguments
+		//check if open session
+
+func check_session(user fstring){
+
+}
+
+func parse_command(cmd fstring){
+	//parse command
+}
+
+func create_player(user fstring){
+	//player creation
+
+}
+
+func get_status(user fstring){
+	// player details
+
+}
+
+func move_area(user fstring, area fstring){
+	// navigation
+
+}
+
+func store(user fstring, action fstring, count int){
+	// store
+
+}
+
+func combat(user fstrung, action fstring, target int){
+	// fight
+
+}
+
+
+func in_team(user fstring){
+	// is the user in a team currently?
+}
+
+
+func get_state(user fstring){
+	//Is the user in town?  Is the user in combat?
+}
+
+	// creature list
