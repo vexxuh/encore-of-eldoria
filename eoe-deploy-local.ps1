439270b1c5d31bdba1401137a7dc5e39f30bee15
shayne-tuel <142040826+shayne-tuel@users.noreply.github.com>
2024-04-30 22:40:14 -0400

    Local Build scripts
    Local build scripts to deploy on unix type systems and windows

---
+++ eoe-deploy-local.ps1
@@ -0,0 +1,8 @@
+# Build Docker image from the current directory
+docker build -t eoe-bot .\discord-bot
+
+# Run Docker image on port 8080:8080
+docker run -p 8080:8080 eoe-bot
+
+# Run encore run command
+encore run
